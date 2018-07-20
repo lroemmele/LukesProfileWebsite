965fe19d71449d34c43cb3eeff03e840eb53a692
lroemmele <lroemmele@gmail.com>
2018-07-20 00:06:34 -0400

    Added webpack, react entry point, bootstrap dark theame

---
+++ profileApp/src/app.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+import Home from './home';
+
+const app =(
+     <Home/>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
---
+++ profileApp/src/home.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Home extends Component{
+     render(){
+          console.log("test");
+          return(
+               <div id="test">
+
+               Hello World</div>
+          );
+     }
+}
+export default Home;
